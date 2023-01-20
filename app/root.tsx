6a07a0c117ed46cf5f7eb856e094090703ecde0b
Guilherme Guerra <guilherme.ga@gmail.com>
2023-01-20 00:05:28 -0300

    Add style to topbar

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,11 @@
 import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -11,9 +13,16 @@ import {
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
+import { Popover } from "@headlessui/react";
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+  return [
+    { rel: "stylesheet", href: tailwindStylesheetUrl },
+    {
+      rel: "stylesheet",
+      href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
+    },
+  ];
 };
 
 export const meta: MetaFunction = () => ({
@@ -36,6 +45,80 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full">
+        <Popover>
+          <header className="flex w-full sm:flex-row flex-col px-4 sm:pt-4 sm:justify-between items-center">
+            <div className="sm:hidden flex w-full justify-between items-center">
+              <div className="flex items-center">
+                <Popover.Button>
+                  <i className="fa-solid fa-bars text-pink"></i>
+                </Popover.Button>
+                <img
+                  className="sm:hidden px-4"
+                  src="./assets/logo-mobile.png"
+                />
+              </div>
+              <div>
+                <Link to="/servicos" className="pl-8">
+                  <i className="fa-brands fa-whatsapp text-green"></i>
+                </Link>
+                <Link to="/servicos" className="pl-4">
+                  <i className="fa-brands fa-linkedin text-green"></i>
+                </Link>
+              </div>
+            </div>
+            <img
+              className="hidden sm:inline-block sm:w-auto"
+              src="./assets/logo.png"
+            />
+
+            <nav className="uppercase hidden sm:flex justify-between items-start">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-pink" : undefined
+                }
+              >
+                home
+              </NavLink>
+              <Link to="/sobre" className="sm:pl-8">
+                sobre
+              </Link>
+              <Link to="/blog" className="sm:pl-8">
+                blog
+              </Link>
+              <Link to="/servicos" className="sm:pl-8">
+                serviços
+              </Link>
+              <Link to="/servicos" className="sm:pl-8 hidden sm:inline-block">
+                <i className="fa-brands fa-whatsapp text-green"></i>
+              </Link>
+              <Link to="/servicos" className="sm:pl-4 hidden sm:inline-block">
+                <i className="fa-brands fa-linkedin text-green"></i>
+              </Link>
+            </nav>
+
+            <Popover.Panel className="flex flex-col uppercase w-full">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-pink" : undefined
+                }
+              >
+                home
+              </NavLink>
+              <Link to="/sobre" className="sm:pl-8">
+                sobre
+              </Link>
+              <Link to="/blog" className="sm:pl-8">
+                blog
+              </Link>
+              <Link to="/servicos" className="sm:pl-8">
+                serviços
+              </Link>
+            </Popover.Panel>
+          </header>
+        </Popover>
+
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {},
+    colors: {
+      pink: "#97538e",
+      green: "#9bb49a"
+    },
+    fontFamily: {
+      sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+    }
   },
   plugins: [],
 };
