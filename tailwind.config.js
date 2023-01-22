7023b516c2f3486544f7a0caaf6b1242940917e9
Guilherme Guerra <guilherme.ga@gmail.com>
2023-01-22 19:55:46 -0300

    Add color palette and custom configuration to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'service-green': "url('/assets/service-green.svg')",
+      },
+      backgroundPosition: {
+        'half': '-270px 110%',
+      }
+    },
     colors: {
-      pink: "#97538e",
-      green: "#9bb49a"
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000000",
+      white: "#ffffff",
+      gray: "#ccc",
+      pink: {
+        DEFAULT: "hsl(308, 29%, 46%)",
+        50: "hsl(308, 29%, 66%)",
+        500: "hsl(308, 29%, 45%)",
+      },
+      green: {
+        DEFAULT: "hsl(118, 15%, 65%)",
+        50: "hsl(118, 15%, 80%)",
+        500: "hsl(118, 15%, 40%)",
+      },
+    },
+    boxShadow: {
+      "custom-pink": "0px 0px 8px 2px rgb(151 83 142 / 20%)",
     },
     fontFamily: {
-      sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
-    }
+      sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 };
