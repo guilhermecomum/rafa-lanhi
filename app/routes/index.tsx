5f9f9fe025dcce796d5e4616d6781e0a64c223e9
Guilherme Guerra <guilherme.ga@gmail.com>
2023-01-23 20:56:38 -0300

    Makes tablet works like mobile

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,16 +2,16 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
-    <main className="relative sm:flex sm:items-center sm:justify-center flex flex-col">
-      <section className="flex flex-col sm:py-10 sm:grid sm:grid-cols-12 sm:px-4 sm:items-center">
+    <main className="relative lg:flex lg:items-center lg:justify-center flex flex-col">
+      <section className="flex flex-col lg:py-10 lg:grid lg:grid-cols-12 lg:px-4 lg:items-center">
         <div className="col-start-2 col-span-4 text-right">
           <img
-            className="object-cover sm:object-none w-full mb-4 h-48 sm:h-auto sm:w-auto sm:rounded-full"
+            className="object-cover lg:object-none w-full mb-4 h-48 lg:h-auto lg:w-auto lg:rounded-full"
             src="./assets/rafa-home.jpg"
           />
         </div>
         <div className="col-start-7 col-span-5 px-4">
-          <h2 className="text-2xl sm:text-4xl text-center font-semibold">
+          <h2 className="text-2xl lg:text-4xl text-center font-semibold">
             Rafaela Lanhi
           </h2>
           <p className="pt-4">
@@ -34,15 +34,15 @@ export default function Index() {
             ampola pa arma uma pindureta.
           </p>
           <div className="text-center mb-8">
-            <button className="bg-pink py-2 w-full sm:w-auto px-4 mt-8 rounded-xl text-white text-xl font-semibold self-center">
+            <button className="bg-pink py-2 w-full lg:w-auto px-4 mt-8 rounded-xl text-white text-xl font-semibold self-center">
               agendar
             </button>
           </div>
         </div>
       </section>
-      <section className="w-full bg-pink lg:bg-white-flower lg:bg-half-center md:h-[80vh] lg:bg-no-repeat flex flex-col text-white">
+      <section className="w-full bg-pink lg:bg-white-flower lg:bg-half-center lg:h-[80vh] lg:bg-no-repeat flex flex-col text-white">
         <div className="w-10/12 mx-auto flex flex-col h-full justify-around">
-          <div className="flex flex-col md:flex-row space-y-16 md:space-y-0 pt-16 md:space-x-16">
+          <div className="flex flex-col lg:flex-row space-y-16 lg:space-y-0 pt-16 lg:space-x-16">
             {[1, 2, 3].map((x, i) => (
               <div key={i} className="flex flex-col">
                 <h2 className="font-semibold">{`Titulo post ${x}`} </h2>
@@ -60,14 +60,14 @@ export default function Index() {
             ))}
           </div>
           <Link
-            className="text-2xl mt-10 mb-8 text-center md:text-right"
+            className="text-2xl mt-10 mb-8 text-center lg:text-right"
             to="/blog"
           >
             ver todos os posts
           </Link>
         </div>
       </section>
-      <section className="bg-green-50 lg:bg-pink-flower lg:bg-half lg:bg-no-repeat sm:grid sm:grid-cols-12 px-4 py-14">
+      <section className="bg-green-50 lg:bg-pink-flower lg:bg-half lg:bg-no-repeat lg:grid lg:grid-cols-12 px-4 py-14">
         <div className="col-span-12 text-center">
           <h2 className="text-4xl text-pink">Acupuntura</h2>
         </div>
