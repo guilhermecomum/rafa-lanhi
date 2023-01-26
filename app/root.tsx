0087671b6e43c3fddfccb10b38d05e90272b7efe
Guilherme Guerra <guilherme.ga@gmail.com>
2023-01-25 23:30:58 -0300

    Remove prisma calls

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,6 @@ import {
 } from "@remix-run/react";
 
 import styles from "~/styles/app.css";
-import { getUser } from "./session.server";
 import { Popover } from "@headlessui/react";
 import { SocialLinks } from "./ui/components/social-links";
 
@@ -49,12 +48,6 @@ export const meta: MetaFunction = () => ({
   title: "Rafaela Lanhi | Fisioterapia & Acupuntura",
   viewport: "width=device-width,initial-scale=1",
 });
-
-export async function loader({ request }: LoaderArgs) {
-  return json({
-    user: await getUser(request),
-  });
-}
 
 export default function App() {
   return (
