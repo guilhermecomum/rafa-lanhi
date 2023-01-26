5d489f32b089ccf1af9d2932e7c7331d0453f766
Guilherme Guerra <guilherme.ga@gmail.com>
2023-01-26 11:15:32 -0300

    Add Google Analytics support

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,6 @@
 import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useEffect } from "react";
 import {
   Links,
   LiveReload,
@@ -6,7 +8,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
+
+import * as gtag from "~/utils/gtags.client";
 
 import styles from "~/styles/app.css";
 import { SocialLinks } from "~/ui/components/social-links";
@@ -46,7 +52,20 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const loader = async () => {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
+};
+
 export default function App() {
+  const location = useLocation();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en" className="h-full scroll-smooth">
       <head>
@@ -54,6 +73,29 @@ export default function App() {
         <Links />
       </head>
       <body className="flex flex-col min-h-screen">
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <Topbar />
         <Outlet />
         <div className="mt-auto bg-pink text-white flex flex-col lg:flex-row space-y-8 text-center lg:space-y-0 lg:text-left justify-between py-8 items-center px-8">
